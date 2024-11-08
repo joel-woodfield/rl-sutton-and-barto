952fb1841cceb2f0c518776b0c60eeccac12f8e0
joel <joelwoodfield@gmail.com>
2024-11-08 15:59:49 +1000

    Update plotting to allow multiple experiments, add major plots

--- multi-armed-bandits/epsilon_greedy_results.py
+++ multi-armed-bandits/epsilon_greedy_results.py
@@ -4,14 +4,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 3:
-    raise ValueError("Requires 2 arguments")
-input_file = sys.argv[1]
+num_experiments = int(sys.argv[1])
 output_file = sys.argv[2]
 
-data = pd.read_csv(input_file).values
-avg_rewards = np.mean(data, axis=0)
-plt.plot(avg_rewards)
+for i in range(num_experiments):
+    input_file = sys.argv[i * 2 + 3]
+    label = sys.argv[i * 2 + 4]
+
+    data = pd.read_csv(input_file).values
+    avg_rewards = np.mean(data, axis=0)
+    plt.plot(avg_rewards, label=label)
+
 plt.xlabel("Timestep")
 plt.ylabel("Average Reward")
+plt.legend()
 plt.savefig(output_file)
